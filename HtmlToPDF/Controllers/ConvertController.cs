da16673a3dfc1139b107d1eb9d8e0c739f1d46d7
adamhockemeyer <hock2k5@gmail.com>
2018-08-01 16:51:05 -0400

    initial

---
+++ HtmlToPDF/Controllers/ConvertController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HtmlToPDF.Controllers
+{
+    [Route("api/[controller]")]
+    public class ConvertController : Controller
+    {
+        private IConverter _converter;
+
+        public ConvertController(IConverter converter)
+        {
+            _converter = converter;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get(string url)
+        {
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    PaperSize = PaperKind.Letter,
+                    Orientation = Orientation.Portrait,
+                    ViewportSize = "1280x1024"
+                }
+                
+            };
+
+            if(!string.IsNullOrEmpty(url))
+            {
+                doc.Objects.Add(new ObjectSettings { Page = url,  });
+            }
+
+            byte[] pdf = _converter.Convert(doc);
+
+
+            return new FileContentResult(pdf, "application/pdf");
+        }
+
+        [HttpPost]
+        public IActionResult Post(string html)
+        {
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    PaperSize = PaperKind.Letter,
+                    Orientation = Orientation.Landscape
+                }
+
+            };
+
+            if (!string.IsNullOrEmpty(html))
+            {
+                doc.Objects.Add(new ObjectSettings {  HtmlContent = html });
+            }
+
+            byte[] pdf = _converter.Convert(doc);
+
+
+            return new FileContentResult(pdf, "application/pdf");
+        }
+    }
+}
---
+++ HtmlToPDF/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HtmlToPDF
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+            HostingEnvironment = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add converter to DI
+            services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
